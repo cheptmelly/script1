77f2ae5dc20df162aaad026f549e06cf399357f8
cheptmelly <82717883+cheptmelly@users.noreply.github.com>
2021-04-21 16:28:56 -0400

    Add files via upload

--- inventory.sh
+++ inventory.sh
@@ -1,7 +1,76 @@
 #!/bin/bash
-#AU: Nancy
+#Author: Nancy M
+#Date: April 21, 2021
 
-if [ $? -eq 0 ]
-then
-echo "success"
-fi
+echo -e "\n Checking the cpu info \n"
+sleep 3
+lscpu
+
+echo -e "\n Checking the hard drive\n"
+sleep 3
+lsblk
+
+echo -e "\n Checking the hard memory\n"
+sleep 3
+free -m
+
+echo -e "\n Checking the system bits\n"
+sleep 3
+getconf LONG_BIT
+
+echo -e "\n Cheking the kernel version\n"
+sleep 3
+uname -r
+
+echo -e "\n Checking the OS version\n"
+sleep 3
+cat /etc/*release
+
+echo -e "\n Checking system bits (32 or 64)\n"
+sleep 3
+getconf LONG_BIT
+
+echo -e "\n Checking the Hostname\n"
+sleep 3
+hostnamectl
+
+echo -e "\n Checking the ip address\n"
+sleep 3
+ifconfig
+
+echo -e "\n Cheking the kernel version\n"
+sleep 3
+uname -r
+
+echo -e "\n Checking the OS version\n"
+sleep 3
+cat /etc/*release
+
+echo -e "\n Checking system bits (32 or 64)\n"
+sleep 3
+getconf LONG_BIT
+
+echo -e "\n Checking the Hostname\n"
+sleep 3
+hostnamectl
+
+echo -e "\n Checking the ip address\n"
+sleep 3
+ifconfig
+
+echo -e "\n Checking all the open ports\n"
+sleep 3
+lsof -i -P -n | grep LISTEN
+
+echo -e "\n Checking the DNS\n"
+sleep 3
+dig tecmint.com
+
+echo -e "\n Checking if the system is virtual or physical\n"
+sleep 3
+lshw |head
+
+echo -e "\n Checking the manufacturer\n"
+sleep 3
+dmidecode -t system
+
